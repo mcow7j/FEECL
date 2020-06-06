908520197da88cc5cf98cadd5103edf346558e63
unknown <mc7315@ic.ac.uk>
2020-06-06 11:09:42 +0100

    ready for release

--- UFL/expr2.py
+++
@@ -1,9 +0,0 @@
-element = FiniteElement("Lagrange", triangle, 1)
-
-u = TrialFunction(element)
-v = TestFunction(element)
-
-f = Coefficent(element)
-kappa = Coefficent(element)
-
-A = kappa*inner(grad(u), curl(v))
